95a0f91d42637c7bcd411dcac32f3deb07143718
megahomyak <g.megahomyak@gmail.com>
2023-12-02 01:41:48 +0500

    Seems like it's done?

---
+++ example.py
@@ -0,0 +1,10 @@
+import worst
+
+def something():
+    a = 123
+    print("Try entering multiline strings using \\")
+    worst.repl()
+    b = 456
+    return 789
+
+print(something())
---
+++ worst/__init__.py
@@ -0,0 +1,24 @@
+import readline as _
+import inspect
+import traceback
+
+def repl():
+    frames_info = inspect.stack()
+    outer_frame = frames_info[1].frame
+    while True:
+        try:
+            i = input(">>> ")
+        except KeyboardInterrupt:
+            break
+        if not i:
+            continue
+        while i.endswith("\\"):
+            i = i[:-1]
+            i += input("  > ")
+        try:
+            try:
+                print(eval(i, outer_frame.f_globals, outer_frame.f_locals))
+            except SyntaxError:
+                print(exec(i, outer_frame.f_globals, outer_frame.f_locals))
+        except:
+            traceback.print_exc(chain=False)
